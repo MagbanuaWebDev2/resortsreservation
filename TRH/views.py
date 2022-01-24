a4f3275fd9c16aa1d1b4e85344de25e2ef81b347
MagbanuaWebDev2 <84842462+MagbanuaWebDev2@users.noreply.github.com>
2022-01-24 21:18:09 +0800

    Update views.py

--- TRH/views.py
+++ TRH/views.py
@@ -94,14 +94,14 @@ def edit(request,id):
 	return render(request,'edit.html',{'card':card})
 
 def update(request,id):
-    card = Card.objects.get(id=id)
-    card.cardname = request.POST['cardname']
-    card.cardnum = request.POST['cardnum']
-    card.expmonth = request.POST['expmonth']
-    card.expyear = request.POST['expyear']
-    card.code = request.POST['code']
-    card.save()
-    return redirect("/carddetails")
+	card = Card.objects.get(id=id)
+	card.cardname = request.POST['cardname']
+	card.cardnum = request.POST['cardnum']
+	card.expmonth = request.POST['expmonth']
+	card.expyear = request.POST['expyear']
+	card.code = request.POST['code']
+	card.save()
+	return redirect("/carddetails")
 
 def destroy(request,id):
 	card = Card.objects.get(id=id)
